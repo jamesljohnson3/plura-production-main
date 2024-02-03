6c7ad8153a85494ae2f6e864151b40b1c9f6d566
Admin <support@unlimitpotential.com>
2024-02-03 18:03:05 -0500

    j

--- lib/definitions.ts
+++ lib/definitions.ts
@@ -7,23 +7,28 @@ import type {
   User,
 } from "@prisma/client";
 
-export type CommentWithExtras = Comment & { user: User };
-export type LikeWithExtras = Like & { user: User };
+export type CommentWithExtras = Comment & { user: User } & { Authuser: User };
+export type LikeWithExtras = Like & { user: User } & { Authuser: User };
 
 export type PostWithExtras = Post & {
   comments: CommentWithExtras[];
   likes: LikeWithExtras[];
   savedBy: SavedPost[];
-  author: User;
+  author: User; // Assuming author is a User
 };
 
-export type FollowerWithExtras = Follows & { follower: User };
-export type FollowingWithExtras = Follows & { following: User };
+export type UserWithFollows = User & {
+  following: FollowingWithExtras[];
+  followedBy: FollowerWithExtras[];
+};
+
+export type FollowerWithExtras = Follows & { follower: UserWithFollows };
+export type FollowingWithExtras = Follows & { following: UserWithFollows };
 
 export type UserWithExtras = User & {
   posts: PostWithExtras[];
   saved: SavedPost[];
   followedBy: FollowerWithExtras[];
   following: FollowingWithExtras[];
-  mylikes: LikeWithExtras[];
+  mylikes: LikeWithExtras[]; // Add the missing property
 };
