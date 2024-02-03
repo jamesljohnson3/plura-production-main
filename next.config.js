08ba6dfbdde4a626916c1ab219624d15139b68ab
Admin <support@unlimitpotential.com>
2024-02-03 01:59:17 -0500

    j

--- next.config.js
+++ next.config.js
@@ -1,22 +1,85 @@
 /** @type {import('next').NextConfig} */
+
+const { createContentlayerPlugin } = require("next-contentlayer");
+
 const nextConfig = {
+  compiler: {
+    removeConsole: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
+
+  reactStrictMode: false,
   images: {
-    domains: [
-      'uploadthing.com',
-      'utfs.io',
-      'img.clerk.com',
-      'subdomain',
-      'files.stripe.com',
-      'res.cloudinary.com',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'uploadthing.com',
+      },
+      {
+        protocol: 'https',
+        hostname:  'utfs.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'files.stripe.com',
+      },
+      {
+        protocol: 'https',
+        hostname:  'subdomain',
+      },
+      {
+        protocol: 'https',
+        hostname:     'utfs.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'hjhncoqotxlxpvrljjgz.supabase.co',
+      },
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        port: "",
+      },
+      
+      {
+        protocol: 'https',
+        hostname: 'unlimitednow.live',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '**'
+      }
     ],
   },
-  reactStrictMode: false,
-}
+ 
+};
 
-module.exports = nextConfig
+
+const withContentlayer = createContentlayerPlugin({
+  // Additional Contentlayer config options
+});
+
+module.exports = withContentlayer(nextConfig);
