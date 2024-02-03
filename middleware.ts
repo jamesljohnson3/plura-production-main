c226a9f621c19bfac567c73af5c73f6889ba63f2
Admin <support@unlimitpotential.com>
2024-02-03 03:44:45 -0500

    j

--- middleware.ts
+++ middleware.ts
@@ -4,15 +4,5 @@ import { NextResponse } from 'next/server'
 
 export default authMiddleware({
   // Specify routes that should be accessible without authentication
-  publicRoutes: ["/", "/login"],
 
-  // This function is called after the authentication middleware is executed
-  async afterAuth(auth, req, evt) {
-    // Allow all logged-in users to access every route
-    if (auth.userId) {
-      return NextResponse.next();
-    }
-    
-    // Redirect non-logged-in users to the sign-in route for other routes
-  },
 });
